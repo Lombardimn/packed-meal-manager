8039173dac7139c719b46b30ce8538e36f37c787
Matias Lombardi <matiiaslombardi@gmail.com>
2024-05-06 01:23:04 -0300

    pagina de registro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { LoginPage } from './pages/login/LoginPage'
+import { RegisterPage } from './pages/register/RegisterPage'
+
 const App = () => {
   return (
-    <>
-      <h1 className="text-4xl font-bold underline">Home Page</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile" element={<h1>Perfil</h1>} />
+        <Route path="/logout" element={<h1>Logout</h1>} />
+        <Route path="/clients" element={<h1>clientes</h1>} />
+        <Route path="/new-client" element={<h1>Nuevo cliente</h1>} />
+        <Route path="/update-client/:id" element={<h1>actualizar cliente</h1>} />
+        <Route path="/query" element={<h1>consulta clientes</h1>} />
+        <Route path="/query/:id" element={<h1>consulta de cliente</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#121212',
+        'secondary': '#dedede',
+        'accent-danger': '#da2735',
+        'danger': '#7f1d1d',
+      }
+    },
   },
   plugins: [],
 }
